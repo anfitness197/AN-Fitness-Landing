0fea72bdf93b7008a5dd45c41c78943a77581cd6
AN Fitness <anfitness197@gmail.com>
2026-07-18 12:55:00 +0530

    sitemap fixes

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-export const runtime = "edge";
+
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://anfitness.in";
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-export const runtime = "edge";
+
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://anfitness.in";
